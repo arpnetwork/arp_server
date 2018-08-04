cb8a8bf31a9166d2f4af757f05761c5325ce4be5
Fludit9 <fludit@arpnetwork.org>
2018-08-04 10:28:45 +0800

    Optimize import keystore

--- lib/arp_server/account.ex
+++ lib/arp_server/account.ex
@@ -11,8 +11,8 @@ defmodule ARP.Account do
     GenServer.start_link(__MODULE__, [], name: __MODULE__)
   end
 
-  def init_key(auth) do
-    GenServer.call(__MODULE__, {:init_key, auth})
+  def init_key(path, auth) do
+    GenServer.call(__MODULE__, {:init_key, path, auth})
   end
 
   def get_info do
@@ -25,10 +25,8 @@ defmodule ARP.Account do
     {:ok, %{private_key: nil, public_key: nil, addr: nil}}
   end
 
-  def handle_call({:init_key, auth}, _from, state) do
-    file_name = File.ls!() |> Enum.find(fn x -> String.slice(x, 0, 4) == "UTC-" end)
-
-    with {:ok, file} <- File.read(file_name),
+  def handle_call({:init_key, path, auth}, _from, state) do
+    with {:ok, file} <- File.read(path),
          {:ok, file_map} <- file |> String.downcase() |> Poison.decode(keys: :atoms),
          {:ok, private_key} <- Crypto.decrypt_keystore(file_map, auth) do
       public_key = ARP.Crypto.eth_privkey_to_pubkey(private_key)
