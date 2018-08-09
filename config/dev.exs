fae1b99869508b6d0a0fcfff7c92012f06bd21c5
Wells Qiu <wellsqiu@arpnetwork.org>
2018-08-09 19:36:11 +0800

    Update `registry_contract_address`

--- config/dev.exs
+++ config/dev.exs
@@ -3,7 +3,7 @@ use Mix.Config
 config :arp_server,
   chain_id: 1000,
   token_contract_address: "0x8d39dd6b431bfb065b51fea07b7ee75bef0b53f8",
-  registry_contract_address: "0xe1c62093f55e8d7a86198dd8186e6de414b3fae4"
+  registry_contract_address: "0xa3159b314777e6529bca4ca2000c6e6456496589"
 
 config :ethereumex,
   url: "http://192.168.0.164:8545"
