4c91d570fa5d6be264a292b1333368547eb2ea38
Fludit9 <fludit@arpnetwork.org>
2018-08-29 15:22:47 +0800

    Optimize CheckTask module

--- lib/arp_server/check_task.ex
+++ lib/arp_server/check_task.ex
@@ -24,20 +24,27 @@ defmodule ARP.CheckTask do
       check_device_bind(device_addr, private_key, server_addr)
     end)
 
+    # check server unregister expired
+    check_server_unregister(private_key, server_addr, device_list)
+
     run()
   end
 
   defp check_dapp_approval(dapp_addr, info, private_key, server_addr) do
     %{id: cid, paid: paid, expired: expired} = ARP.Contract.bank_allowance(dapp_addr, server_addr)
 
-    check_time = expired - 60 * 60 * 24
+    %{expired: bind_expired} = ARP.Contract.get_dapp_bind_info(dapp_addr, server_addr)
+
+    check_time = bind_expired - 60 * 60 * 24
+    allowance_check_time = expired - 60 * 60 * 24
     now = DateTime.utc_now() |> DateTime.to_unix()
 
-    if expired != 0 && now > check_time do
+    if bind_expired != 0 && now > check_time do
       ARP.Device.release_by_dapp(dapp_addr)
     end
 
-    if info["cid"] == cid && info["amount"] > paid && expired != 0 && now > check_time do
+    if info["cid"] == cid && info["amount"] > paid &&
+         ((bind_expired != 0 && now > check_time) || (expired != 0 && now > allowance_check_time)) do
       Task.start(fn ->
         ARP.Contract.bank_cash(private_key, dapp_addr, server_addr, info["amount"], info["sign"])
       end)
@@ -52,6 +59,27 @@ defmodule ARP.CheckTask do
     if server == server_addr && expired != 0 && expired < now do
       Task.start(fn -> ARP.Contract.unbind_device_by_server(private_key, device_addr) end)
       ARP.DevicePromise.delete(device_addr)
+    end
+  end
+
+  defp check_server_unregister(private_key, server_addr, device_list) do
+    now = DateTime.utc_now() |> DateTime.to_unix()
+
+    %{ip: ip, expired: expired, size: size} = ARP.Contract.get_registered_info(server_addr)
+
+    if ip != 0 && expired != 0 && now > expired do
+      if size == 0 do
+        Task.start(fn -> ARP.Contract.unregister(private_key) end)
+      else
+        Enum.each(device_list, fn device_addr ->
+          %{server: server, expired: device_expired} =
+            ARP.Contract.get_device_bind_info(device_addr)
+
+          if server == server_addr && device_expired == 0 do
+            Task.start(fn -> ARP.Contract.unbind_device_by_server(private_key, device_addr) end)
+          end
+        end)
+      end
     end
   end
 end
